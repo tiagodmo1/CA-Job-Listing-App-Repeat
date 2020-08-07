fde664dcd92d74e877969fc2bf548196fe9c8533
tiagodmo1 <tiagodmo@yahoo.com.br>
2020-08-07 23:00:39 +0530

    JS Changes

--- app/view/js/demo.js
+++ app/view/js/demo.js
@@ -16,7 +16,7 @@ $("#submit").prop('disabled', false);
 $("#delete").click(function(){
 var dataId=$("#dataId").val();
 $.ajax({
-            url: 'https://bookartapp.herokuapp.com/api/bookkart/'+dataId,
+            url: 'https://joblistingappnew.herokuapp.com/api/bookkart/'+dataId,
             type: 'delete',
             dataType: 'json',
              
@@ -51,7 +51,7 @@ var dataJson = {
         };
 
 $.ajax({
-            url: 'http://bookartapp.herokuapp.com/api/bookkart/'+dataId,
+            url: 'http://joblistingappnew.herokuapp.com/api/bookkart/'+dataId,
             type: 'put',
             dataType: 'json',
              data: JSON.stringify(dataJson),
@@ -89,7 +89,7 @@ var dataJson = {
 console.log(JSON.stringify(dataJson));
 
 $.ajax({
-            url: 'https://bookartapp.herokuapp.com/api/bookkart',
+            url: 'https://joblistingappnew.herokuapp.com/api/bookkart',
             type: 'post',
             dataType: 'json',
              data: JSON.stringify(dataJson),
@@ -123,7 +123,7 @@ $("#example").show();
 $('#table1').DataTable().destroy();
 
   $.ajax({
-    type: "get", url: "https://bookartapp.herokuapp.com/api/bookkart",
+    type: "get", url: "https://joblistingappnew.herokuapp.com/api/bookkart",
     success: function (data, text) {
       var table= $('#example').DataTable( {
         "data": data,
@@ -156,7 +156,7 @@ $("#submit").prop('disabled', true);
        
 $("#dataId").val(data2._id);
   $.ajax({
-    type: "get", url: "https://bookartapp.herokuapp.com/api/bookkart/"+data2._id,
+    type: "get", url: "https://joblistingappnew.herokuapp.com/api/bookkart/"+data2._id,
     success: function (data, text) {
 
            $("#section").val(data.section);
